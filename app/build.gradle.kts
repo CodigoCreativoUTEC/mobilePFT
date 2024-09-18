1e478ae1ffbacfcc240741544e8da49ae38114e9
melanie.llorente <melanie.llorente@estudiantes.utec.edu.uy>
2024-09-18 15:07:11 -0300

    Saco lo de firebase que no va. Inicio de diseño de registro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -46,6 +46,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.firebase.bom)
-    implementation(libs.firebase.analytics)
 }
