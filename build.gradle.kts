119e9c78d44aec7ba04173b40a6ffa452a838b0c
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-11-01 21:46:28 -0300

    Update build.gradle.kts
    sonar conf

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,15 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-
+id("org.sonarqube") version "5.1.0.4882"
 
 }
+
+sonar {
+  properties {
+    property("sonar.projectKey", "CodigoCreativoUTEC_mobilePFT_ac8d7ecb-83c0-42cf-986e-373f612278a8")
+    property("sonar.projectName", "mobilePFT")
+  }
 
 buildscript {
     repositories {
