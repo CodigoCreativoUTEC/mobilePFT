1ff3cfcdf59153454768db8947a2b7720cadaf2a
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-11-01 22:34:13 -0300

    Update build.gradle.kts
    arreglos en archivo

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,21 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-id("org.sonarqube") version "5.1.0.4882"
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 sonar {
-  properties {
-    property("sonar.projectKey", "CodigoCreativoUTEC_mobilePFT_ac8d7ecb-83c0-42cf-986e-373f612278a8")
-    property("sonar.projectName", "mobilePFT")
-  }
+    properties {
+        property("sonar.projectKey", "CodigoCreativoUTEC_mobilePFT_ac8d7ecb-83c0-42cf-986e-373f612278a8")
+        property("sonar.projectName", "mobilePFT")
+    }
+}
 
 buildscript {
     repositories {
         google()
     }
     dependencies {
-        classpath(libs.google.services)
+       classpath(libs.google.services)
     }
 }
-
